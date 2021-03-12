e6c2a000128a6a6dbf63bdec2f580d5830474749
punscake <51837195+punscake@users.noreply.github.com>
2021-03-12 18:26:24 -0500

    0.01

--- Source/Spaceypede/PedeSegment.cpp
+++ Source/Spaceypede/PedeSegment.cpp
@@ -9,7 +9,23 @@ APedeSegment::APedeSegment()
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	
+	CollisionNose = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionNose"));
+	CollisionNose->InitSphereRadius(40.0f);
+	CollisionNose->SetCollisionProfileName(TEXT("CollisionNoseHitbox"));
 
+	CollisionButt = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionButt"));
+	CollisionButt->InitSphereRadius(40.0f);
+	CollisionButt->SetCollisionProfileName(TEXT("CollisionButtHitbox"));
+
+	CollisionContactDamage = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionContactDamage"));
+	CollisionContactDamage->InitSphereRadius(40.0f);
+	CollisionContactDamage->SetCollisionProfileName(TEXT("CollisionContactDamageHitbox"));
+
+	isPossessed = false;
+	isRogue = true;
+	baseSpeed = 10.0;
+	speedBoostModifier = 1.0;
+	speedHaltModifier = 1.0;
 }
 
 // Called when the game starts or when spawned
@@ -17,6 +33,9 @@ void APedeSegment::BeginPlay()
 {
 	Super::BeginPlay();
 	
+
+	
+
 }
 
 // Called every frame
@@ -24,12 +43,73 @@ void APedeSegment::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	/*
+	* calculate speed
+	* if possesed
+	*	turn based on turn rate
+	*	blink forward based on speed
+	* 
+	*	leave trail for connected segment
+	*	report distance traveled to connected segment
+	* else if has trail
+	*	get distance traveled, continue if more than 0
+	*	get last trail from stack
+	*	calculate distance to trail
+	* 
+	*	if distance traveled < distance to trail
+	*		move distance traveled towards trail
+	*		put trail back
+	*	else if distance traveled = distance to trail
+	*		move to trail
+	*	else
+	*		distanc traveled -= distance to trail
+	*		move to trail
+	*		repeat the if statement
+	*	
+	*	leave trail for connected segment
+	*	report distance traveled to connected segment
+	* 
+	* else
+	*	physics for rogue objects
+	*/
 }
 
 // Called to bind functionality to input
 void APedeSegment::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	InputComponent->BindAction("SpeedUp", IE_Pressed, this, &APedeSegment::BoostSpeed);
+	InputComponent->BindAction("SpeedUp", IE_Released, this, &APedeSegment::DeboostSpeed);
+	InputComponent->BindAction("SlowDown", IE_Pressed, this, &APedeSegment::Halt);
+	InputComponent->BindAction("SlowDown", IE_Released, this, &APedeSegment::UnHalt);
+
+
+	//InputComponent->BindAction("Shoot", IE_Pressed, this, &APedeSegment::);
+}
+
+void APedeSegment::BoostSpeed() {
+	speedBoostModifier = 2.0;
+}
+
+void APedeSegment::DeboostSpeed() {
+	speedBoostModifier = 1.0;
+}
+
+void APedeSegment::Halt() {
+	speedHaltModifier = 0.0;
+}
+
+void APedeSegment::UnHalt() {
+	speedHaltModifier = 1.0;
+}
+
+float APedeSegment::CalculateSpeed() {
+	float resultingSpeed = baseSpeed;
+	return baseSpeed * speedBoostModifier * speedHaltModifier;
+}
+
+void APedeSegment::Turn() {
 
 }
 
--- Source/Spaceypede/PedeSegment.h
+++ Source/Spaceypede/PedeSegment.h
@@ -4,18 +4,101 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "Containers/Queue.h"
+#include "Components/SphereComponent.h"
+
 #include "PedeSegment.generated.h"
+
+using namespace std;
 
 UCLASS()
 class SPACEYPEDE_API APedeSegment : public APawn
 {
 	GENERATED_BODY()
 
-
+		
 
 public:
 	// Sets default values for this pawn's properties
 	APedeSegment();
+
+	class TQueue<FVector, EQueueMode::Mpsc> FVectorTrailQueue;
+	//TODO location+rotation
+
+	//TODO mesh
+
+	//TODO ActionComponent
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	class USphereComponent* CollisionNose;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	class USphereComponent* CollisionButt;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	class USphereComponent* CollisionContactDamage;
+
+	class APedeSegment* Leads;
+
+	class APedeSegment* LedBy;
+
+	/*methods:
+	* connectTo(PedeSegment* otherSegment) - if(otherSegment.getIsRogue) generates an initial trailQueue, otherSegment.setLeads(this), calls passControl
+	* setLedBy(PedeSegment* LedBy)
+	* getLedBy()
+	* setLeads(PedeSegment* Leads)
+	* getLeads()
+	* getIsRogue()
+	* passControl(PedeSegment* otherSegment) - unbinds player controlled and camera, this.isPossessed = false, calls otherSegment.recieveControl(PlayerController?)
+	* recieveControl(PlayerController?) - binds player controlled and camera, clears up trailQueue, removes dependancy LedBy, isPossessed = true, isRogue = false
+	* disconnect() - LedBy.setLeads(null), clean up trailQueue, if (!this.isPossessed) set this and the rest of segments rogue=true (or disconnect?)
+	* 
+	* move() - all move logic each frame
+	* turn()
+	* forward()
+	* followTrail()
+	* leaveTrail()
+	*/
+
+protected:
+
+	UPROPERTY(VisibleAnywhere)
+		bool isPossessed; //possessed by a player
+
+	UPROPERTY(VisibleAnywhere)
+		bool isRogue;  //no = belongs to a controlled train
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly) //TODO change to visible only
+		float baseSpeed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		float speedBoostModifier; //multiplier for accelerated movement (usually gets set to 2.0)
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		float speedHaltModifier; //multiplier for halting (usually gets set to 0)
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		float turnRate;
+
+private:
+
+	UFUNCTION()
+	void Turn();
+
+	UFUNCTION()
+	void BoostSpeed();
+
+	UFUNCTION()
+	void DeboostSpeed();
+
+	UFUNCTION()
+	void Halt();
+
+	UFUNCTION()
+	void UnHalt();
+
+	UFUNCTION()
+	float CalculateSpeed(); //uses baseSpeed, speed modifiers and properties of segments attached to give final movement speed
 
 protected:
 	// Called when the game starts or when spawned
